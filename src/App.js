eedc98d6b8641d57170bb6b5e96d420f735e0fb1
Adebara Ayomide <adebaraayomide@gmail.com>
2023-03-01 21:12:42 +0100

    Ayomide: App.js updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import "./App.css";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
 import DriverUpload from "./Components/DriverUpload";
+import IncomingTripPage from "./pages/IncomingTripPage";
+import DriverDashBoardPage from "./pages/DriverDashBoardPage";
+import UserDepositPage from "./pages/UserDepositPage";
+import Review from "./pages/Review";
+import AddReviewer from "./pages/AddReviewer";
+import DriverApplicationPage from "./pages/DriverApplicationPage";
+import PassengerDashboard from "./pages/PassengerDashboard";
+import PassengerPayment from "./pages/PassengerPayment";
+import TripRequest from "./pages/TripRequest";
+import AdminDashboard from "./pages/AdminDashboard";
+import RenderChart from "./Components/RenderChart";
+import ApplicationStatus from "./pages/ApplicationStatus";
+import PassengerReview from "./pages/PassengerReview";
+import DriverTripHistory from "./pages/DriverTripHistory";
+import Trip from "./pages/Trip";
 
 function App() {
-  return <DriverUpload />;
+  return <DriverApplicationPage />;
 }
 
 export default App;
